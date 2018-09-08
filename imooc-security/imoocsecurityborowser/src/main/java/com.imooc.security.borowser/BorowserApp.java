61e09de2df36bc456f16ea6d2597a63acd14ed82
zhang1github2test <760965963@qq.com>
2018-09-08 21:42:41 +0800

    '新增四个自项目'

---
+++ imooc-security/imoocsecurityborowser/src/main/java/com.imooc.security.borowser/BorowserApp.java
@@ -0,0 +1,14 @@
+package com.imooc.security.borowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * zhshl  2018/9/8
+ */
+@SpringBootApplication
+public class BorowserApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BorowserApp.class);
+    }
+}
---
+++ imooc-security/imoocsecuritycore/src/main/java/com/imooc/security/core/ImoocSecurityCoreApp.java
@@ -0,0 +1,14 @@
+package com.imooc.security.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * zhshl  2018/9/8
+ */
+@SpringBootApplication
+public class ImoocSecurityCoreApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocSecurityCoreApp.class);
+    }
+}
